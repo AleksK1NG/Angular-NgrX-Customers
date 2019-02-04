4cef6d57c4c18950ba307d36d53d4e556d58c1a2
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-02-04 19:15:09 +0300

    customer load reducer and actions

--- src/app/customers/state/customer.actions.ts
+++ src/app/customers/state/customer.actions.ts
@@ -1,1 +1,27 @@
-export const test = () => test
+import { Customer } from '../customer.model';
+import { Action } from '@ngrx/store';
+
+export enum CustomerActionsTypes {
+  LOAD_CUSTOMERS = '[Customer] Load Customers',
+  LOAD_CUSTOMERS_SUCCESS = '[Customer] Load Customers Success',
+  LOAD_CUSTOMERS_FAIL = '[Customer] Load Customers Fail',
+}
+
+
+export class LoadCustomers implements Action {
+  readonly type = CustomerActionsTypes.LOAD_CUSTOMERS;
+}
+
+export class LoadCustomersSuccess implements Action {
+  readonly type = CustomerActionsTypes.LOAD_CUSTOMERS_SUCCESS;
+
+  constructor(public payload: Customer[]) {}
+}
+
+export class LoadCustomersFail implements Action {
+  readonly type = CustomerActionsTypes.LOAD_CUSTOMERS_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export type Action = LoadCustomers | LoadCustomersSuccess | LoadCustomersFail;
--- src/app/customers/state/customer.reducer.ts
+++ src/app/customers/state/customer.reducer.ts
@@ -1,24 +1,47 @@
-const initialState = {
-  customers: [
-    {
-      name: 'John Doe',
-      phone: '910928392098',
-      address: '123 Sun Street',
-      membership: 'Platinum',
-      id: 1
-    }
-  ],
+import * as fromRoot from '../../state/app-state';
+import { Customer } from '../customer.model';
+import * as customerActions from './customer.actions';
+
+export interface CustomerState {
+  customers: Customer[];
+  loading: boolean;
+  loaded: boolean;
+  error: string;
+}
+
+export interface AppState extends fromRoot.AppState {
+  customers: CustomerState;
+}
+
+export const initialState: CustomerState = {
+  customers: [],
   loading: false,
-  loaded: true
+  loaded: true,
+  error: ''
 };
 
-export function customerReducer(state = initialState, action) {
+export function customerReducer(state = initialState, action: customerActions.Action): CustomerState {
   switch (action.type) {
-    case 'LOAD_CUSTOMERS': {
+    case customerActions.CustomerActionsTypes.LOAD_CUSTOMERS: {
       return {
         ...state,
-        loading: true,
-        loaded: false
+        loading: true
+      };
+    }
+    case customerActions.CustomerActionsTypes.LOAD_CUSTOMERS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        customers: action.payload
+      };
+    }
+    case customerActions.CustomerActionsTypes.LOAD_CUSTOMERS_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        customers: [],
+        error: action.payload
       };
     }
     default: {
@@ -26,3 +49,33 @@ export function customerReducer(state = initialState, action) {
     }
   }
 }
+
+
+// const initialState = {
+//   customers: [
+//     {
+//       name: 'John Doe',
+//       phone: '910928392098',
+//       address: '123 Sun Street',
+//       membership: 'Platinum',
+//       id: 1
+//     }
+//   ],
+//   loading: false,
+//   loaded: true
+// };
+//
+// export function customerReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case 'LOAD_CUSTOMERS': {
+//       return {
+//         ...state,
+//         loading: true,
+//         loaded: false
+//       };
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// }
